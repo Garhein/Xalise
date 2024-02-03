608f268849e237c5cfe5475648cb182e934326e0
Xavier VILLEMIN <xavier.villemin@gmail.com>
2024-02-03 16:06:41 +0100

    Finalisation du segment MSH

--- Xalise.Interop/HL7/Structure/Segment/MSH.cs
+++ Xalise.Interop/HL7/Structure/Segment/MSH.cs
@@ -26,7 +26,7 @@ namespace Xalise.Interop.HL7.Structure.Segment
             this.InitField(typeof(HD), "Sending Facility", 227, 1, EnumDataUsage.OPTIONAL, TableDefinition.T0362_FACILITY);
             this.InitField(typeof(HD), "Receiving Application", 227, 1, EnumDataUsage.OPTIONAL, TableDefinition.T0361_APPLICATION);
             this.InitField(typeof(HD), "Receiving Facility", 227, 1, EnumDataUsage.OPTIONAL, TableDefinition.T0362_FACILITY);
-            this.InitField(typeof(TS), "Receiving Facility", 26, 1, EnumDataUsage.REQUIRED);
+            this.InitField(typeof(TS), "Date/Time Of Message", 26, 1, EnumDataUsage.REQUIRED);
             this.InitField(typeof(ST), "Security", 40, 1, EnumDataUsage.OPTIONAL);
             this.InitField(typeof(MSG), "Message Type", 15, 1, EnumDataUsage.REQUIRED);
             this.InitField(typeof(ST), "Message Control ID", 20, 1, EnumDataUsage.REQUIRED);
@@ -44,7 +44,7 @@ namespace Xalise.Interop.HL7.Structure.Segment
         }
 
         /// <summary>
-        /// Récupère le champ <c>Field Separator</c> (MSH-1)..
+        /// Récupère le champ <c>Field Separator</c> (MSH-1).
         /// </summary>
         public ST FieldSeparator
         {
@@ -55,6 +55,468 @@ namespace Xalise.Interop.HL7.Structure.Segment
                 try
                 {
                     ret = (ST)this.GetField(1, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Encoding Characters</c> (MSH-2).
+        /// </summary>
+        public ST EncodingCharacters
+        {
+            get
+            {
+                ST ret = null;
+
+                try
+                {
+                    ret = (ST)this.GetField(2, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Sending Application</c> (MSH-3).
+        /// </summary>
+        public HD SendingApplication
+        {
+            get
+            {
+                HD ret = null;
+
+                try
+                {
+                    ret = (HD)this.GetField(3, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Sending Facility</c> (MSH-4).
+        /// </summary>
+        public HD SendingFacility
+        {
+            get
+            {
+                HD ret = null;
+
+                try
+                {
+                    ret = (HD)this.GetField(4, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Receiving Application</c> (MSH-5).
+        /// </summary>
+        public HD ReceivingApplication
+        {
+            get
+            {
+                HD ret = null;
+
+                try
+                {
+                    ret = (HD)this.GetField(5, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Receiving Facility</c> (MSH-6).
+        /// </summary>
+        public HD ReceivingFacility
+        {
+            get
+            {
+                HD ret = null;
+
+                try
+                {
+                    ret = (HD)this.GetField(6, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Date/Time Of Message</c> (MSH-7).
+        /// </summary>
+        public TS DateTimeOfMessage
+        {
+            get
+            {
+                TS ret = null;
+
+                try
+                {
+                    ret = (TS)this.GetField(7, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Security</c> (MSH-8).
+        /// </summary>
+        public ST Security
+        {
+            get
+            {
+                ST ret = null;
+
+                try
+                {
+                    ret = (ST)this.GetField(8, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Message Type</c> (MSH-9).
+        /// </summary>
+        public MSG MessageType
+        {
+            get
+            {
+                MSG ret = null;
+
+                try
+                {
+                    ret = (MSG)this.GetField(9, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Message Control ID</c> (MSH-10).
+        /// </summary>
+        public ST MessageControlID
+        {
+            get
+            {
+                ST ret = null;
+
+                try
+                {
+                    ret = (ST)this.GetField(10, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Processing ID</c> (MSH-11).
+        /// </summary>
+        public PT ProcessingID
+        {
+            get
+            {
+                PT ret = null;
+
+                try
+                {
+                    ret = (PT)this.GetField(11, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Version ID</c> (MSH-12).
+        /// </summary>
+        public VID VersionID
+        {
+            get
+            {
+                VID ret = null;
+
+                try
+                {
+                    ret = (VID)this.GetField(12, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Sequence Number</c> (MSH-13).
+        /// </summary>
+        public NM SequenceNumber
+        {
+            get
+            {
+                NM ret = null;
+
+                try
+                {
+                    ret = (NM)this.GetField(13, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Continuation Pointer</c> (MSH-14).
+        /// </summary>
+        public ST ContinuationPointer
+        {
+            get
+            {
+                ST ret = null;
+
+                try
+                {
+                    ret = (ST)this.GetField(14, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Accept Acknowledgment Type</c> (MSH-15).
+        /// </summary>
+        public ID AcceptAcknowledgmentType
+        {
+            get
+            {
+                ID ret = null;
+
+                try
+                {
+                    ret = (ID)this.GetField(15, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Accept Acknowledgment Type</c> (MSH-16).
+        /// </summary>
+        public ID ApplicationAcknowledgmentType
+        {
+            get
+            {
+                ID ret = null;
+
+                try
+                {
+                    ret = (ID)this.GetField(16, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Country Code</c> (MSH-17).
+        /// </summary>
+        public ID CountryCode
+        {
+            get
+            {
+                ID ret = null;
+
+                try
+                {
+                    ret = (ID)this.GetField(17, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère une répétition précise du champ <c>Character Set</c> (MSH-18).
+        /// </summary>
+        /// <param name="rep">Numéro de la répétition.</param>
+        /// <returns></returns>
+        public ID GetCharacterSet(int rep)
+        {
+            ID ret = null;
+
+            try
+            {
+                ret = (ID)this.GetField(18, rep);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Récupère toutes les répétitions du champ <c>Character Set</c> (MSH-18).
+        /// </summary>
+        /// <returns></returns>
+        public ID[] GetCharacterSet()
+        {
+            ID[] ret = null;
+
+            try
+            {
+                IType[] reps = this.GetField(18);
+
+                ret = new ID[reps.Length];
+
+                for (int i = 0; i < ret.Length; i++)
+                {
+                    ret[i] = (ID)reps[i];
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Récupère le nombre de répétitions du champ <c>Character Set</c> (MSH-18).
+        /// </summary>
+        public int CharacterSetTotalRepetitions
+        {
+            get
+            {
+                try
+                {
+                    return this.GetTotalFieldRepetitions(18);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Principal Language Of Message</c> (MSH-19).
+        /// </summary>
+        public CE PrincipalLanguageOfMessage
+        {
+            get
+            {
+                CE ret = null;
+
+                try
+                {
+                    ret = (CE)this.GetField(19, 1);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Récupère le champ <c>Alternate Character Set Handling Scheme</c> (MSH-20).
+        /// </summary>
+        public ID AlternateCharacterSetHandlingScheme
+        {
+            get
+            {
+                ID ret = null;
+
+                try
+                {
+                    ret = (ID)this.GetField(20, 1);
                 }
                 catch (Exception)
                 {
@@ -114,7 +576,7 @@ namespace Xalise.Interop.HL7.Structure.Segment
         }
 
         /// <summary>
-        /// Récupère le nombre de répétitions du champ <c>Message Profile Identifier"</c> (MSH-21).
+        /// Récupère le nombre de répétitions du champ <c>Message Profile Identifier</c> (MSH-21).
         /// </summary>
         public int MessageProfileIdentifierTotalRepetitions
         {
