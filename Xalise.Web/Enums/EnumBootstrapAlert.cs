e4336f5b0f193090cd47401a63e57e9eae4dfa3b
Xavier VILLEMIN <xavier.villemin@gmail.com>
2024-07-20 11:48:20 +0200

    Mise en place du tag "xalise-alert"

---
+++ Xalise.Web/Enums/EnumBootstrapAlert.cs
@@ -0,0 +1,27 @@
+﻿using Xalise.Core.Attributes;
+
+namespace Xalise.Web.Enums
+{
+    /// <summary>
+    /// Alerte Bootstrap.
+    /// </summary>
+    public enum eBootstrapAlert
+    {
+        [CssClassName("alert-primary")]
+        alert_primary,
+        [CssClassName("alert-secondary")]
+        alert_secondary,
+        [CssClassName("alert-success")]
+        alert_success,
+        [CssClassName("alert-danger")]
+        alert_danger,
+        [CssClassName("alert-warning")]
+        alert_warning,
+        [CssClassName("alert-info")]
+        alert_info,
+        [CssClassName("alert-light")]
+        alert_light,
+        [CssClassName("alert-dark")]
+        alert_dark
+    }
+}
---
+++ Xalise.Web/Helpers/TagHelpers/XaliseAlertTagHelper.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text.Encodings.Web;
+using Xalise.Core.Extensions;
+using Xalise.Web.Enums;
+using Xalise.Web.Helpers.WebHelpers;
+
+namespace Xalise.Web.Helpers.TagHelpers
+{
+    /// <summary>
+    /// Alerte bootstrap pouvant contenir une icône fontawesome.
+    /// </summary>
+    public class XaliseAlertTagHelper : TagHelper
+    {
+        public string                   CssClasses { get; set; }
+        public eBootstrapAlert          AlertStyle { get; set; }
+        public eFontAwesomeIconStyle?   IconStyle { get; set; }
+        public eFontAwesomeIcon?        Icon { get; set; }
+
+        /// <summary>
+        /// Constructeur par défaut.
+        /// </summary>
+        public XaliseAlertTagHelper()
+        {
+            this.CssClasses = string.Empty;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            // =-=-=-
+            // Définition des classes CSS
+            // =-=-=-
+
+            string cssClasses = this.CssClasses;
+            
+            if (cssClasses.IsNotNullOrWhiteSpace())
+            {
+                cssClasses += " ";
+            }
+
+            cssClasses += $"alert {this.AlertStyle.CssClassName()}";
+
+            // =-=-=-
+            // Gestion des classes CSS supplémentaires
+            // =-=-=-
+
+            if (this.Icon.HasValue && this.IconStyle.HasValue)
+            {
+                cssClasses += " d-flex align-items-center justify-content-center";
+            }
+            else
+            {
+                cssClasses += " text-center";
+            }
+
+            // =-=-=-
+            // Définition du contenu
+            // =-=-=-
+
+            if (this.Icon.HasValue && this.IconStyle.HasValue)
+            {
+                // Avec icône
+                HtmlString iconHtml = new HtmlString("");
+                TagBuilder faIcon   = new TagBuilder("i");
+                faIcon.AddCssClass(FontAwesomeHelper.FontAwesomeClass(this.IconStyle.Value, this.Icon.Value) + " flex-shrink-0 me-2");
+
+                using (StringWriter writer = new StringWriter())
+                {
+                    faIcon.WriteTo(writer, HtmlEncoder.Default);
+                    iconHtml = new HtmlString(writer.ToString());
+                }
+
+                output.PreContent.AppendHtml(iconHtml);
+            }
+
+            // =-=-=-
+            // Gestion finale du tag
+            // =-=-=-
+
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.TagName = "div";
+            output.Attributes.SetAttribute("role", "alert");
+            output.Attributes.SetAttribute("class", cssClasses);
+        }
+    }
+}
